cb80abd8492226e20c764191031fc2b061dd7449
stripe-ci <support@stripe.com>
2019-08-12 11:25:07 -0700

    Brew formula update for stripe version v0.3.0

--- stripe.rb
+++ stripe.rb
@@ -2,15 +2,15 @@
 class Stripe < Formula
   desc "Stripe CLI utility"
   homepage "https://stripe.com"
-  version "0.2.7"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/stripe/stripe-cli/releases/download/v0.2.7/stripe_0.2.7_mac-os_x86_64.tar.gz"
-    sha256 "f6ba49251938572933ceb91c152453834fc65e51f1b384ffcf29b6bbc55cf484"
+    url "https://github.com/stripe/stripe-cli/releases/download/v0.3.0/stripe_0.3.0_mac-os_x86_64.tar.gz"
+    sha256 "d004709ccb96cddc85a569a4c119fefc98a81b28e071bdc883cee94da2bb364f"
   elsif OS.linux?
-    url "https://github.com/stripe/stripe-cli/releases/download/v0.2.7/stripe_0.2.7_linux_x86_64.tar.gz"
-    sha256 "0861ec0a5e50d18234e6ff7e549e20a32c8d6d12242fc958597f2b7ed4d59d2e"
+    url "https://github.com/stripe/stripe-cli/releases/download/v0.3.0/stripe_0.3.0_linux_x86_64.tar.gz"
+    sha256 "2832aa3988b38a1b5b6ee38e0c1a6f3184c18b9a8043c0faa809c9ebe58a0191"
   end
 
   def install
