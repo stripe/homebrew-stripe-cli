e30077b5e9d3cd6d0f8ab1ffb941db379c11abb0
stripe-ci <support@stripe.com>
2019-11-11 12:12:41 -0800

    Brew formula update for stripe version v1.2.1

--- stripe.rb
+++ stripe.rb
@@ -2,16 +2,16 @@
 class Stripe < Formula
   desc "Stripe CLI utility"
   homepage "https://stripe.com"
-  version "1.2.0"
+  version "1.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/stripe/stripe-cli/releases/download/v1.2.0/stripe_1.2.0_mac-os_x86_64.tar.gz"
-    sha256 "68201087761ecca154abf55225eeb592e15e8f7959f7f51431db4aacd8d90c4e"
+    url "https://github.com/stripe/stripe-cli/releases/download/v1.2.1/stripe_1.2.1_mac-os_x86_64.tar.gz"
+    sha256 "1f68a84bb5a73c5471edd4dbe3a3259cec48f6912069ddbaf0e7294b6a8622e0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/stripe/stripe-cli/releases/download/v1.2.0/stripe_1.2.0_linux_x86_64.tar.gz"
-      sha256 "cb38daca7e4d6bec49d791d93c3330ddaa78073ff7b622acc884ce3c86c5bd05"
+      url "https://github.com/stripe/stripe-cli/releases/download/v1.2.1/stripe_1.2.1_linux_x86_64.tar.gz"
+      sha256 "fa4a40c12aec0e890581a6370d25affb26a791f8fc91966b8183c95c0d7fa6e0"
     end
   end
 
