b1d7fc4ad9c43730e9c91c7302acd53d7b255066
stripe-ci <support@stripe.com>
2023-06-27 14:45:51 -0400

    Brew formula update for stripe version v1.15.0

--- stripe.rb
+++ stripe.rb
@@ -5,13 +5,13 @@
 class Stripe < Formula
   desc "Stripe CLI utility"
   homepage "https://stripe.com"
-  version "1.14.7"
+  version "1.15.0"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stripe/stripe-cli/releases/download/v1.14.7/stripe_1.14.7_mac-os_arm64.tar.gz"
-      sha256 "2cf037fda11cb3ca64031cf7361a861f877027e9cea76bc9ec0422e35b32aaea"
+      url "https://github.com/stripe/stripe-cli/releases/download/v1.15.0/stripe_1.15.0_mac-os_arm64.tar.gz"
+      sha256 "41151d4c2496a7501226fb1be8277bd21c826d4b539a984ffda286bc56e6a34b"
 
       def install
         bin.install "stripe"
@@ -31,8 +31,8 @@ class Stripe < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/stripe/stripe-cli/releases/download/v1.14.7/stripe_1.14.7_mac-os_x86_64.tar.gz"
-      sha256 "fc4a887e81aa8a8d3e69fb2dd11ff54e3d0437dcb3c67628d559f0b3f77232a1"
+      url "https://github.com/stripe/stripe-cli/releases/download/v1.15.0/stripe_1.15.0_mac-os_x86_64.tar.gz"
+      sha256 "4761bd1a3eaf3a72381b06feffba90ff57e5743188b3a6e54898db52b693e28f"
 
       def install
         bin.install "stripe"
