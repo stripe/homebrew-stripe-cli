4964ebd67ce034dc585f2a46c81dc7b6d2bfde33
stripe-ci <support@stripe.com>
2025-01-21 20:35:19 -0500

    Brew formula update for stripe version v1.23.4

--- stripe.rb
+++ stripe.rb
@@ -5,34 +5,13 @@
 class Stripe < Formula
   desc "Stripe CLI utility"
   homepage "https://stripe.com"
-  version "1.23.3"
+  version "1.23.4"
   depends_on :macos
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/stripe/stripe-cli/releases/download/v1.23.3/stripe_1.23.3_mac-os_arm64.tar.gz"
-      sha256 "bb19e9e8e73bb81c4df683edebadefaca8c8ba023fd2a75947c84fa9c1cc6de7"
-
-      def install
-        bin.install "stripe"
-        rm Dir["#{bin}/{stripe-completion.bash,stripe-completion.zsh}"]
-        system bin/"stripe", "completion", "--shell", "bash"
-        system bin/"stripe", "completion", "--shell", "zsh"
-        bash_completion.install "stripe-completion.bash"
-        zsh_completion.install "stripe-completion.zsh"
-        (zsh_completion/"_stripe").write <<~EOS
-          #compdef stripe
-          _stripe () {
-            local e
-            e=$(dirname ${funcsourcetrace[1]%:*})/stripe-completion.zsh
-            if [[ -f $e ]]; then source $e; fi
-          }
-        EOS
-      end
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/stripe/stripe-cli/releases/download/v1.23.3/stripe_1.23.3_mac-os_x86_64.tar.gz"
-      sha256 "31b0ee23964b6b38d8a1e8efa3fde0202aa15c778f815f68da8d71d9c3c8a68c"
+      url "https://github.com/stripe/stripe-cli/releases/download/v1.23.4/stripe_1.23.4_mac-os_x86_64.tar.gz"
+      sha256 "bcd70cf108df0ed623e4eba4a020107d567933e4088a0e96a11a97f950f2313d"
 
       def install
         bin.install "stripe"
